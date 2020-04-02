99a808f90d02af412d05d8d4e08708f6602bed97
renna3 <63052880+renna3@users.noreply.github.com>
2020-04-02 14:53:04 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,19 +4,6 @@ namespace prodotto_ricorsione
 {
     class Program
     {
-
-        public static int prodotto(int p, int m)
-        { 
-            if (p == 1)
-                return m;
-
-            else {
-                p--;
-                return m + prodotto(p, m);
-                
-            }
-        }      
-
         public static int potenza(int b, int e)
         {
             if (e == 1)
@@ -27,35 +14,10 @@ namespace prodotto_ricorsione
                 e--;
                 return b * potenza(b, e);
             }
-
         }
-
-
-
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("scegliere:");
-            Console.WriteLine("1) prodotto");
-            Console.WriteLine("2) elevamento");
-            int scelta = Convert.ToInt32(Console.ReadLine());
-
-
-            switch (scelta)
-            {
-
-                case 1:
-
-                    Console.WriteLine("inserire il numero");
-                    int n1 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("inserire il moltiplicatore");
-                    int n2 = Convert.ToInt32(Console.ReadLine());
-
-                    int prod = prodotto(n1, n2);
-                    Console.WriteLine(prod);
-
-                    break;
-
-                case 2:
                     Console.WriteLine("inserire il numero");
                     int n3 = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("inserire l'elevamento");
@@ -63,11 +25,6 @@ namespace prodotto_ricorsione
 
                     int pot = potenza(n3, n4);
                     Console.WriteLine(pot);
-
-                    break;
-
-
-
             }
         }
     }
